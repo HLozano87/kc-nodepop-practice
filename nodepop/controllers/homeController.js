0e654b834ba8e70f05bcfbfce97cfb5ab9b95a34
Hector Lozano <hectorlg87@gmail.com>
2025-03-29 01:52:27 +0100

    feat: implementar gestión de sesiones y control de acceso a productos por usuario autenticado

--- nodepop/controllers/homeController.js
+++ nodepop/controllers/homeController.js
@@ -3,9 +3,10 @@ import Product from "../models/Product.js"
 /* GET home page. */
 export const index = async (req, res, next) => {
   try {
-    const products = await Product.find()
+    const userId = req.session.userId
+    const products = await Product.find({ owner: userId })
     res.locals.products = products
-    res.render('index', { products })
+    res.render('index')
   } catch (error) {
     next(error)
   }
---
+++ nodepop/lib/sessionManager.js
@@ -0,0 +1,28 @@
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import { config } from 'dotenv'
+
+config()
+
+const SECRET_SESSION = process.env.SECRET_SESSION || 'Secret'
+const TIME_LIFE_SESSION = parseInt(process.env.TIME_LIFE_SESSION, 10) ?? (2 * 24 * 60 * 60 * 1000) // 2 Days
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/nodepop'
+
+export const sessionUser = session({
+    name: 'nodepop-sessionUsers',
+    secret: SECRET_SESSION,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+      maxAge: TIME_LIFE_SESSION
+    },
+    store: MongoStore.create({
+      mongoUrl: MONGO_URL
+    })
+})
+
+export function useSessionUsersInViews(req, res, next) {
+  res.locals.session = req.session
+  next()
+}
+
