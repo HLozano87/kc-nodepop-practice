598f5c6e1f37d48349baaffe9715284f6336b684
Hector Lozano <hectorlg87@gmail.com>
2025-03-30 20:56:00 +0200

    feat: mejorar la validación de parámetros en la creación de productos y actualizar el formulario de login

--- nodepop/controllers/productController.js
+++ nodepop/controllers/productController.js
@@ -3,14 +3,18 @@ import Product from '../models/Product.js'
 
 export const validateParams = [
 
-  body('name').isString().withMessage('El nombre del producto es obligatorio'),
-  body('price').isNumeric({ min: 0 }).withMessage('El precio debe ser un número mayor a 0'),
+  body('name')
+    .matches(/^([a-zA-Z]+|\w+\d)(\s([a-zA-Z]+|\w+\d))*$/)
+    .isString()
+    .withMessage('El nombre del producto es obligatorio, y debe ser valido. Ej: Producto 1 o Producto.'),
+  body('price')
+    .custom( value => value > 0 ).withMessage('El precio debe ser un número mayor a 0'),
   body('image').optional().isString().withMessage('La URL de la imagen debe ser válida'),
   body('tags').isString().withMessage('Las etiquetas deben ser un arreglo de strings'),
   (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return ({ errors: errors.array() })
+      return res.render('form', { errors: errors.array().map(e => e.msg) })
     }
     next()
   }
