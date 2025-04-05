22a08fe91b6806fa1eff08e1b9b89a403283c702
Hector Lozano <hectorlg87@gmail.com>
2025-04-06 00:06:36 +0200

    feat: se añade indice al schema de producto para optimizar busquedas y poder filtrar

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose'
 
 const productSchema = new Schema({
   name: String,
-  owner: { ref: 'User', type: mongoose.Schema.Types.ObjectId },
+  owner: { index: true, ref: 'User', type: mongoose.Schema.Types.ObjectId },
   price: Number,
   image: String,
   tags: [ String ]
